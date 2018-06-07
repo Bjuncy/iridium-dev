95b20fd4778ca78f2eac7243984b64366359fd59
Ushio <jylzs369@163.com>
2018-06-07 16:39:54 +0800

    补充示例

--- src/components/demo/DemoForms.vue
+++ src/components/demo/DemoForms.vue
@@ -1,81 +1,80 @@
 <template>
   <div class="iridium-demo-forms">
-    <div class="block">
-      <h3>默认表单</h3>
-      <form class="iridium-form">
+    <h3>默认表单</h3>
+    <form class="iridium-form">
+      <div class="iridium-form-item">
         <input class="iridium-form-input" />
-        <input class="iridium-form-input" disabled="disabled" /><br>
+      </div>
+      <div class="iridium-form-item">
+        <input class="iridium-form-input" disabled="disabled" />
+      </div>
+      <div class="iridium-form-item">
         <label for="input" class="iridium-form-label">label：</label>
         <input id="input" class="iridium-form-input" />
+      </div>
+      <div class="iridium-form-item">
         <textarea class="iridium-form-textarea">多行文本框</textarea>
-      </form>
-    </div>
-    <div class="block">
-      <h3>垂直表单</h3>
-      <form class="iridium-form iridium-form--vertical">
-        <label class="iridium-form-label">
-          label：
-        </label>
+      </div>
+    </form>
+    <pre>&lt;form class="iridium-form"&gt;
+  &lt;div class="iridium-form-item"&gt;
+    &lt;input class="iridium-form-input" /&gt;
+  &lt;/div&gt;
+  &lt;div class="iridium-form-item"&gt;
+    &lt;input class="iridium-form-input" disabled="disabled" /&gt;
+  &lt;/div&gt;
+  &lt;div class="iridium-form-item"&gt;
+    &lt;label for="input" class="iridium-form-label"&gt;label：&lt;/label&gt;
+    &lt;input id="input" class="iridium-form-input" /&gt;
+  &lt;/div&gt;
+  &lt;div class="iridium-form-item"&gt;
+    &lt;textarea class="iridium-form-textarea"&gt;多行文本框&lt;/textarea&gt;
+  &lt;/div&gt;
+&lt;/form&gt;</pre>
+    <h3>垂直表单</h3>
+    <form class="iridium-form iridium-form--vertical">
+      <div class="iridium-form-item">
+        <label class="iridium-form-label">label：</label>
         <input class="iridium-form-input" />
-      </form>
-      <form class="iridium-form iridium-form--vertical">
-        <label for="textarea3" class="iridium-form-label">
-          label：
-        </label>
-        <textarea id="textarea3" class="iridium-form-textarea" />
-      </form>
-    </div>
-    <div class="block">
-      <h3>水平表单</h3>
-      <form class="iridium-form iridium-form--horizontal">
+      </div>
+      <div class="iridium-form-item">
+        <label class="iridium-form-label">label：</label>
+        <textarea class="iridium-form-textarea" />
+      </div>
+    </form>
+    <pre>&lt;form class="iridium-form iridium-form--vertical"&gt;
+  &lt;div class="iridium-form-item"&gt;
+    &lt;label class="iridium-form-label"&gt;label：&lt;/label&gt;
+    &lt;input class="iridium-form-input" /&gt;
+  &lt;/div&gt;
+  &lt;div class="iridium-form-item"&gt;
+    &lt;label class="iridium-form-label"&gt;label：&lt;/label&gt;
+    &lt;textarea class="iridium-form-textarea"&gt;&lt;/textarea&gt;
+  &lt;/div&gt;
+&lt;/form&gt;</pre>
+    <h3>水平表单</h3>
+    <form class="iridium-form iridium-form--horizontal">
+      <div class="iridium-form-item">
+        <label class="iridium-form-label">label：</label>
+        <input class="iridium-form-input" disabled="disabled" />
+      </div>
+      <div class="iridium-form-item">
         <label class="iridium-form-label">
           label：
         </label>
-        <input class="iridium-form-input" disabled="disabled" />
-      </form>
-      <form class="iridium-form iridium-form--horizontal">
-        <label for="textarea3" class="iridium-form-label">
-          label：
-        </label>
-        <textarea id="textarea3" class="iridium-form-textarea disabled" />
-      </form>
-    </div>
-    <div class="block">
-      <h3>居中表单</h3>
-      <form class="iridium-form center">
-        <label class="iridium-form-label">
-          label：
-        </label>
-        <input class="iridium-form-input" disabled="disabled" />
-      </form>
-      <form class="iridium-form iridium-form--vertical center">
-        <label class="iridium-form-label">
-          label：
-        </label>
-        <input class="iridium-form-input" disabled="disabled" />
-      </form>
-      <form class="iridium-form iridium-form--horizontal center">
-        <label class="iridium-form-label">
-          label：
-        </label>
-        <input class="iridium-form-input" disabled="disabled" />
-      </form>
-    </div>
-    <div class="block">
-      <h3>下拉菜单</h3>
-      <select class="iridium-select">
-        <option class="iridium-select-option" value="1">11</option>
-        <option class="iridium-select-option" value="1">11</option>
-        <option class="iridium-select-option" value="1">11</option>
-      </select>
-      <div class="iridium-select"></div>
-      <ul class="iridium-dropdown">
-        <li class="iridium-dropdown-option">111</li>
-        <li class="iridium-dropdown-option">111</li>
-        <li class="iridium-dropdown-option">111</li>
-      </ul>
-      <div class="iridium-select disabled"></div>
-    </div>
+        <textarea class="iridium-form-textarea" />
+      </div>
+    </form>
+    <pre>&lt;form class="iridium-form iridium-form--horizontal"&gt;
+  &lt;div class="iridium-form-item"&gt;
+    &lt;label class="iridium-form-label"&gt;label：&lt;/label&gt;
+    &lt;input class="iridium-form-input" disabled="disabled" /&gt;
+  &lt;/div&gt;
+  &lt;div class="iridium-form-item"&gt;
+    &lt;label class="iridium-form-label"&gt;label：&lt;/label&gt;
+    &lt;textarea class="iridium-form-textarea"&gt;&lt;/textarea&gt;
+  &lt;/div&gt;
+&lt;/form&gt;</pre>
   </div>
 </template>
 
--- src/components/demo/DemoHeadings.vue
+++ src/components/demo/DemoHeadings.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="iridium-demo-headdings">
-    <div class="block">
-      <h1>标题</h1>
-      <h2>标题</h2>
-      <h3>标题</h3>
-      <h4>标题</h4>
-      <h5>标题</h5>
-      <h6>标题</h6>
-    </div>
+    <h1>H1标题</h1>
+    <h2>H2标题</h2>
+    <h3>H3标题</h3>
+    <h4>H4标题</h4>
+    <h5>H5标题</h5>
+    <h6>H6标题</h6>
+    <pre>&lt;h1&gt;H1标题&lt;/h1&gt;
+&lt;h2&gt;H2标题&lt;/h2&gt;
+&lt;h3&gt;H3标题&lt;/h3&gt;
+&lt;h4&gt;H4标题&lt;/h4&gt;
+&lt;h5&gt;H5标题&lt;/h5&gt;
+&lt;h6&gt;H6标题&lt;/h6&gt;</pre>
   </div>
 </template>
 
