ddf579800acf11a0e442334e2ebd112a2f038fe0
B05FCH60.黄碧君 <v-huangbj@vanke.net.cn>
2018-06-13 11:22:10 +0800

    基本完成DailySheet页面，新增月报表接口文档

--- src/components/views/dailySheet.vue
+++ src/components/views/dailySheet.vue
@@ -5,26 +5,27 @@
         <div class="iridium-daily-sheet-label">
           <div class="iridium-daily-sheet-label-name">移动标签：</div>
           <div class="iridium-daily-sheet-item">
-            <select class="iridium-select">
-              <option class="iridium-select-option" value="">请选择</option>
-              <option class="iridium-select-option" value="2">选项</option>
-              <option class="iridium-select-option" value="3">选项</option>
+            <select class="iridium-select" v-model="labels">
+              <option class="iridium-select-option" value="请选择">请选择</option>
+              <option class="iridium-select-option" :key="val.labelId" :value="val.name" v-for="val in labelOptions">{{ val.name }}</option>
             </select>
           </div>
           <div class="iridium-daily-sheet-item">
-            <select class="iridium-select">
-              <option class="iridium-select-option" value="金色家园">金色家园</option>
-              <option class="iridium-select-option" value="惠山花园">惠山花园</option>
+            <select class="iridium-select" v-model="project">
+              <option class="iridium-select-option" :key="val.projectId" :value="val.name" v-for="val in projectOptions">{{ val.name }}</option>
             </select>
           </div>
         </div>
         <div class="iridium-daily-sheet-label">
           <div class="iridium-daily-sheet-date iridium-daily-sheet-item">
-            <DatePicker type="date"></DatePicker>
+            <div class="iridium-daily-sheet-date-picker">
+              <DatePicker ref="picker" type="date" :value="dateVal" :editable="editable" @on-change="handleChange" :open="open" @on-open-change="handleOpenChange"></DatePicker>
+            </div>
+            <span @click="handleCalendar" class="iridium-daily-sheet-calendar-icon"><i class="fa fa-calendar"></i></span>
           </div>
           <div class="iridium-daily-sheet-btn-box">
             <button class="iridium-btn iridium-btn--primary iridium-btn--smaller iridium-daily-sheet-btn">查询</button>
-            <button class="iridium-btn iridium-btn--danger iridium-btn--smaller iridium-daily-sheet-btn">导出</button>
+            <button class="iridium-btn iridium-btn--danger iridium-btn--smaller iridium-daily-sheet-btn"><a class="aDownload" href="#" download="../../assets/images/logo-nav-hide.png">导出</a></button>
           </div>
         </div>
         <div class="iridium-daily-sheet-tabel">
@@ -77,15 +78,35 @@
         <div class="iridium-daily-sheet-pages flex">
           <div class="flex-1 left">
             <p v-if="!totalNum">显示0项</p>
-            <p else>显示{{currentPage}}到{{currentPage*10}}条，共{{totalNum}}条</p>
+            <p else>显示{{(currentPage-1)*10+1}}到{{currentPage*10}}条，共{{totalNum}}条</p>
           </div>
           <div class="flex-1">
             <ul class="flex right">
-              <li><a href="javascript:void(0);" class="iridium-daily-sheet-pages-item iridium-daily-sheet-leftborder iridium-daily-sheet-lradius" :class="{aDisable:!pages}">首页</a></li>
-              <li><a href="#" class="iridium-daily-sheet-pages-item">上一页</a></li>
-              <li :key="index" v-for="(n,index) in pages"><a href="#" class="iridium-daily-sheet-pages-item" :class="{aCurrent:n == currentPage}">{{n}}</a></li>
-              <li><a href="#" class="iridium-daily-sheet-pages-item">下一页</a></li>
-              <li><a href="#" class="iridium-daily-sheet-pages-item iridium-daily-sheet-rradius">末页</a></li>
+              <li @click="handlePages(1)">
+                <a href="javascript:void(0);"
+                  class="iridium-daily-sheet-pages-item iridium-daily-sheet-leftborder iridium-daily-sheet-lradius"
+                  :class="{ aDisable: !pages, aCurrent: currentPage == 1}">首页</a>
+              </li>
+              <li @click="handlePages(currentPage-1)">
+                <a href="javascript:void(0);"
+                  class="iridium-daily-sheet-pages-item"
+                  :class="{ aDisable: pages && pages === 1 || pages && currentPage === 1 || !pages }">上一页</a>
+              </li>
+              <li @click="handlePages(n)" :key="index" v-for="( n, index ) in pages">
+                <a href="javascript:void(0);"
+                  class="iridium-daily-sheet-pages-item"
+                  :class="{ aCurrent:n == currentPage }">{{ n }}</a>
+              </li>
+              <li @click="handlePages(currentPage+1)">
+                <a href="javascript:void(0);"
+                  class="iridium-daily-sheet-pages-item"
+                  :class="{ aDisable: pages && pages === 1 || pages && currentPage === pages || !pages }">下一页</a>
+              </li>
+              <li @click="handlePages(pages)">
+                <a href="javascript:void(0);"
+                  class="iridium-daily-sheet-pages-item iridium-daily-sheet-rradius"
+                  :class="{ aDisable: pages && pages === 1 || !pages, aCurrent: currentPage == pages}">末页</a>
+              </li>
             </ul>
           </div>
         </div>
@@ -103,6 +124,51 @@ export default {
   },
   data () {
     return {
+      labels: '请选择',
+      project: '沈阳金域蓝湾（北区）',
+      dateVal: '',
+      editable: false,
+      open: false,
+      isCalendar: true,
+      isDate: false,
+      labelOptions: [
+        {
+          'labelId': '01',
+          'code': '01,01',
+          'name': 'A区地库岗',
+          'type': '移动标签'
+        },
+        {
+          'labelId': '02',
+          'code': '01,02',
+          'name': 'B区地库岗',
+          'type': '移动标签'
+        }
+      ],
+      projectOptions: [
+        {
+          'projectId': '00',
+          'code': '21010011',
+          'name': '沈阳金域蓝湾（北区）',
+          'title': '沈阳金域蓝湾（北区）',
+          'address': '沈阳市浑南新区朗日街1号',
+          'price': '10.66',
+          'leader': '柳丽霞',
+          'phone': '15142077879',
+          'supplier': '济宁亚旗物业服务有限公司'
+        },
+        {
+          'projectId': '01',
+          'code': '21010005',
+          'name': '金色家园',
+          'title': '金色家园',
+          'address': '沈阳市大东区八王寺街28号万科金色家园',
+          'price': '9.26',
+          'leader': '高会涛',
+          'phone': '18202416225',
+          'supplier': '济宁亚琪物业服务有限公司'
+        }
+      ],
       list: [
         {
           'labelId': '01',
@@ -133,7 +199,53 @@ export default {
       totalNum: 34,
       currentPage: 1
     }
-  }
+  },
+  methods: {
+    handlePages (n) {
+      if (this.pages) {
+        if (n >= this.pages) {
+          this.currentPage = this.pages
+        } else if (n <= 1) {
+          this.currentPage = 1
+        } else {
+          this.currentPage = n
+        }
+        console.log(this.currentPage)
+      }
+    },
+    handleChange (date) {
+      this.dateVal = date
+      this.open = false
+    },
+    handleOpenChange (e) {
+      if (e) {
+        this.open = true
+        this.isDate = true
+      } else {
+        if (this.isCalendar && !this.isDate) {
+          this.open = true
+          this.isCalendar = false
+        } else {
+          this.open = false
+        }
+      }
+    },
+    handleCalendar () {
+      this.open = true
+      this.isCalendar = true
+      this.isDate = false
+    }
+   
+  },
+  watch: {
+    'labels' (newVal, oldVal) {
+      console.log(newVal, oldVal)
+    },
+    'project' (newVal, oldVal) {
+      console.log(newVal, oldVal)
+    }
+  },
+  
 }
 </script>
 <style scoped>
@@ -169,7 +281,27 @@ export default {
       padding: 0 var(--fontBase);
     }
     @descendent date {
-      padding-left: 94px;
+      margin-left: 80px;
+      position:relative;
+    }
+    @descendent date-picker {
+      flex:1;
+      box-sizing:border-box;
+      position:relative;
+      margin-right:40px;
+    }
+    @descendent calendar-icon {
+      width:40px;
+      background:#fff;
+      position:absolute;
+      right:14px;
+      top:0;
+      bottom:0;
+      text-align:center;
+      border:1px solid var(--borderColor);
+      line-height:32px;
+      box-sizing:border-box;
+      border-left:0;
     }
     @descendent btn {
       margin: 0 2px;
@@ -189,6 +321,8 @@ export default {
       padding:2px 8px;
       border:1px solid var(--borderColor);
       border-left:0;
+      height:20px;
+      line-height:20px;
     }
     @descendent leftborder{
       border-left:1px solid var(--borderColor);
