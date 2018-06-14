b175e301ae05971e3d6f9e95fd77ce870b2166b4
Ushio <jylzs369@163.com>
2018-06-14 17:32:52 +0800

    dropdown样式修改

--- src/components/universal/CommonDropdown.vue
+++ src/components/universal/CommonDropdown.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="iridium-dropdown">
-    <i :class=""></i>
-    <span>{{ title }}</span>
-    <ul>
-      <li v-for="(item, index) in datalist" :key="index" @click="to(item)">{{ item.text }}</li>
+    <span class="iridium-dropdown-title" @click="toggleSlide">{{ title }} <i :class="icon"></i></span>
+    <ul v-show="showList" class="iridium-dropdown-list">
+      <li v-for="(item, index) in datalist" :key="index" class="iridium-dropdown-option" :class="{'iridium-dropdown-option--division': item.division}" @click="select($event, item)">{{ item.text }}</li>
     </ul>
   </div>
 </template>
@@ -14,7 +13,7 @@ export default {
   props: {
     icon: {
       type: String,
-      default: ''
+      default: 'fa fa-angle-down'
     },
     title: {
       type: String,
@@ -29,11 +28,18 @@ export default {
       }
     }
   },
+  data () {
+    return {
+      showList: false
+    }
+  },
   methods: {
-    to (item) {
-      item.href && this.$router.push({
-        name: item.href
-      })
+    toggleSlide () {
+      this.showList = !this.showList
+    },
+    select (event, item) {
+      this.$emit('select', { $event: event, option: item })
+      this.showList = false
     }
   }
 }
