41b15a5a028913ad5eb263d1f92b9458a90f202a
Ushio <jylzs369@163.com>
2018-06-21 17:43:24 +0800

    新增loading组件

--- src/iview.js
+++ src/iview.js
@@ -1,4 +1,4 @@
-import { Row, Col, Message, Modal, Notice } from 'iview'
+import { Row, Col, Message, Modal, Notice, Spin } from 'iview'
 import 'iview/dist/styles/iview.css'
 
 // 全局注册iview组件
@@ -9,9 +9,11 @@ export default {
     Vue.component('Message', Message)
     Vue.component('Modal', Modal)
     Vue.component('Notice', Notice)
+    Vue.component('Spin', Spin)
     Vue.prototype.$message = Message
     Vue.prototype.$modal = Modal
     Vue.prototype.$notice = Notice
+    Vue.prototype.$spin = Spin
 
     Vue.prototype.$message.config({
       top: 200,
