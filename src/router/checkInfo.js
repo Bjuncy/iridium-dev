53493ae50c882eeb1ec97b8772fb4059b697743c
Bjuncy <820352137@qq.com>
2018-06-26 17:20:10 +0800

    新增抽查信息管理router

---
+++ src/router/checkInfo.js
@@ -0,0 +1,7 @@
+import MoveTabs from '@/components/views/MoveTabs'
+import CheckInfomationInput from '@/components/views/CheckInfomationInput'
+
+export default [
+  { path: '/move-tabs', name: 'MoveTabs', component: MoveTabs },
+  { path: '/check-infomation-input', name: 'CheckInfomationInput', component: CheckInfomationInput }
+]
